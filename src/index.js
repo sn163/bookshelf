98a55f1c59ff1ffff508fe7c86a9feed0e8c4100
Sonny Nguyen <sonnynguyen163@gmail.com>
2022-12-22 17:08:03 -0800

    Revert "Exercises 01 bootstrap and extras completed"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-// export * from './index.final'
+import {loadDevTools} from './dev-tools/load'
+import './bootstrap'
+import * as React from 'react'
+import {createRoot} from 'react-dom/client'
+import {Profiler} from 'components/profiler'
+import {App} from './app'
+import {AppProviders} from './context'
 
-export * from './index.exercise'
-
-// 💯 Use `@reach/dialog`
-// export * from './index.extra-1'
-
-// 💯 Create a LoginForm component
-// export * from './index.extra-2'
+loadDevTools(() => {
+  createRoot(document.getElementById('root')).render(
+    <Profiler id="App Root" phases={['mount']}>
+      <AppProviders>
+        <App />
+      </AppProviders>
+    </Profiler>,
+  )
+})
